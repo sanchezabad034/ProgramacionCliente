e2a409d27d3ff12a1f8df06a4224ddfc45e94e84
sanchezabad034 <68512055+sanchezabad034@users.noreply.github.com>
2022-03-21 22:51:08 -0600

    update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "ba5300e4d974af",
+	password: "e913cea1",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_8a8e65c7440b65a",
 	secret_key:"ashryputrtlopa1jki"
 };
 exports.config = config;
